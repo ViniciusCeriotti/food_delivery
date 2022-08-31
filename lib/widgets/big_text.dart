6277eb27fda876fdaea0ef946f8e4c7991cc2ef0
¨Vinícius <vini.ceriotti@hotmail.com>
2022-08-31 17:04:25 -0300

    Project created

---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BigText extends StatelessWidget {
+  final Color? color;
+  final String text;
+  double size;
+  TextOverflow overFlow;
+  BigText({super.key, required this.color, required this.text, this.size=20, this.overFlow=TextOverflow.ellipsis});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      overflow: overFlow,
+      style: TextStyle(
+        fontFamily: 'Roboto',
+        color: color,
+        fontWeight: FontWeight.w400
+      )
+    );
+  }
+}
